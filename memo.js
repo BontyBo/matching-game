1ea676e460181dd4110ee842f7ea0705846139d8
BontyBo <gabane1950ultrapromax@gmail.com>
2024-09-27 14:08:22 +0700

    add difficulty level

--- memo.js
+++ memo.js
@@ -3,12 +3,22 @@ let clicked1 = [];
 let clicked2 = [];
 let board = [];
 let paired = [];
+let difficulty = 'normal';//easy2 normal4 hard8
+let nimY;
+if(difficulty == 'easy'){nimY = 2;}
+else if(difficulty == 'normal'){nimY = 4;}
+else if(difficulty == 'hard'){nimY = 8;}
 
+//2/4/8
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background('white');
-    let numbers = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9];
-    for(let i = 0; i<4; i++){
+    let numbers = [];
+    for(let i = 0; i<nimY*5/2; i++){
+        numbers.push(i);
+        numbers.push(i);
+    }
+    for(let i = 0; i<nimY; i++){
         board.push([]);
         for(let j = 0; j<5; j++){
             const randomNum = random(numbers);
@@ -27,33 +37,33 @@ function draw() {
     let text1;
     let text2;
     const blockX = floor(windowWidth/5);
-    const blockY = floor(windowHeight/4);
+    const blockY = floor(windowHeight/nimY);
     for(let i=1; i<5; i++){
-        line(iwindowWidth/5, 0, iwindowWidth/5, windowHeight);
+        line(i*windowWidth/5, 0, i*windowWidth/5, windowHeight);
     }
-    for(let i=1; i<4; i++){
-        line(0, iwindowHeight/4, windowWidth, iwindowHeight/4);
+    for(let i=1; i<nimY; i++){
+        line(0, i*windowHeight/nimY, windowWidth, i*windowHeight/nimY);
     }
     if(clicked1.length!=0){
         text1 = board[clicked1[0]][clicked1[1]];
-        text(text1.toString(), clicked1[1]blockX+blockX/2, clicked1[0]blockY+blockY/2);
+        text(text1.toString(), clicked1[1]*blockX+blockX/2, clicked1[0]*blockY+blockY/2);
     }
     if(clicked2.length!=0){
         text2 = board[clicked2[0]][clicked2[1]];
-        text(text2.toString(), clicked2[1]blockX+blockX/2, clicked2[0]blockY+blockY/2);
+        text(text2.toString(), clicked2[1]*blockX+blockX/2, clicked2[0]*blockY+blockY/2);
         if(text1 == text2){paired.push([clicked1[0],clicked1[1],clicked2[0],clicked2[1]]);}
     }
     paired.map((axis) => {
         const numshow = board[axis[0]][axis[1]].toString();
-        text(numshow, axis[1]blockX+blockX/2, axis[0]blockY+blockY/2);
-        text(numshow, axis[3]blockX+blockX/2, axis[2]blockY+blockY/2);
+        text(numshow, axis[1]*blockX+blockX/2, axis[0]*blockY+blockY/2);
+        text(numshow, axis[3]*blockX+blockX/2, axis[2]*blockY+blockY/2);
     });
 }
 
 function mouseClicked(){
     if(clicked2.length == 0){
         const blockX = floor(windowWidth/5);
-        const blockY = floor(windowHeight/4);
+        const blockY = floor(windowHeight/nimY);
         const arrayX = floor(mouseX/blockX);
         const arrayY = floor(mouseY/blockY);
         if(clicked1.length == 0){clicked1.push(arrayY,arrayX);}
